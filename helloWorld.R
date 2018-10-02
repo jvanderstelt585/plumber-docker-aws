1e6b5d1f7aeaf368cfc559d786bd96ff14b388fe
Joseph Vanderstelt <36106368+jvanderstelt585@users.noreply.github.com>
2018-10-02 10:08:32 -0500

    Update helloWorld.R

--- helloWorld.R
+++ helloWorld.R
@@ -1,19 +1,3 @@
-library(jsonlite)
-
-hostName <- system("hostname", intern = TRUE)
-
-#* @get /api/ping
-ping <- function()
-{
- #environmentSettings <- Sys.getenv()
- #hostName <- Sys.getenv("COMPUTERNAME")
- #if (hostName == ""){
- hostName <- system("hostname", intern = TRUE)
- #}
- echoText <- sprintf("[%s]: Welcome to Muni Horizon Pricing Model!", hostName)
- return(echoText)
-}
-
 #* @filter cors
 cors <- function(res) {
   res$setHeader("Access-Control-Allow-Origin", "*")
@@ -32,4 +16,3 @@ function(req) {
   return(theResponse)
   
 }
-
